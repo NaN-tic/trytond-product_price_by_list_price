f7fae22ce1627c645f2886404173c041b3b40668
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:30:59 +0100

    Cleanup tests

--- product.py
+++ product.py
@@ -1,6 +1,6 @@
-#This file is part product_price_by_list_price module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part product_price_by_list_price module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import ModelView, fields
 from trytond.wizard import Wizard, StateView, StateAction, Button
 from trytond.pool import PoolMeta
